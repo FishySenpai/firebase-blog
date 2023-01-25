6a2c2fa2f33f2262117aee9ac1de9de3ca37e933
FishySenpai <akanomanbasit@gmail.com>
2023-01-25 22:34:20 +0500

    basic setup

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_waP36y5ppMLAWx3lA0whsDpZPQhtsFE",
+  authDomain: "fir-blog-20192.firebaseapp.com",
+  projectId: "fir-blog-20192",
+  storageBucket: "fir-blog-20192.appspot.com",
+  messagingSenderId: "43924930444",
+  appId: "1:43924930444:web:321ee7d7d0f60985e960a9",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
