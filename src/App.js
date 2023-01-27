9c0f9f3afa1a63a0c799ec66a94b66e768782524
FishySenpai <akanomanbasit@gmail.com>
2023-01-27 22:34:31 +0500

    adf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        sagfdsg
-      </header>
+      
     </div>
   );
 }
